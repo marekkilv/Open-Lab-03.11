a578bc35d6c28ea4cec8777891f7128125059926
Marekmp@outlook.sk <Marekmp@outlook.sk>
2021-11-29 19:46:06 +0100

    mam

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -7,6 +7,30 @@ namespace Open_Lab_03._11
         public bool IsPalindrome(string str)
         {
             throw new NotImplementedException();
+              int Number_Of_Matching_Char = 0;
+            int Position_In_Array = 0;
+            char[] characters = str.ToCharArray();
+
+            foreach (char item in characters)
+            {
+                if (characters[Position_In_Array] == characters[Position_In_Array + 1])
+                {
+                    Number_Of_Matching_Char += 1;
+                }
+                if (Position_In_Array < characters.Length - 2) 
+                { 
+                    Position_In_Array += 1;
+                } 
+            }
+
+            if (Number_Of_Matching_Char > 0) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         } 
     }
 }
